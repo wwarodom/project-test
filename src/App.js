3261443faf0c73672d979220812b4b87904ee7cc
pat <patnarin241240@gmail.com>
2019-08-17 14:52:11 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'antd/es/button';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Signin from './pages/Signin'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+
+        <Router
+          exact
+          path="/signin"
+          component={Signin}
+        />
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Signin extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+export default Signin
